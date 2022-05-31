825fc3b6687f3c519a5073e07dd9eb3c83e3c674
Tuan Tran <langthamht047@gmail.com>
2022-05-31 08:06:04 -0700

    First Commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>Copyright 2022 by Coder Wikipedia</footer>
+  )
+}
---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+     <Head>
+      <title>Coder Wikipedia | About</title>
+      <meta name ="keywords" content="about"/>
+    </Head>
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam minus,
+        exercitationem excepturi sit magni enim adipisci debitis deleniti atque
+        impedit corporis quas inventore dolorum itaque, iste, labore animi.
+        Illum, iste!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam minus,
+        exercitationem excepturi sit magni enim adipisci debitis deleniti atque
+        impedit corporis quas inventore dolorum itaque, iste, labore animi.
+        Illum, iste!
+      </p>
+      
+    </div>
+    </>
+  );
+}
